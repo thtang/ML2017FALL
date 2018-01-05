3f567e8482f257c3ffecac1c8ce32cb91db31308
thtang <penniless1994@gmail.com>
2018-01-05 13:47:01 +0800

    add pca.sh pca_generate.py

---
+++ hw6/pca.sh
@@ -0,0 +1,1 @@
+python3 pca_generate.py $1 $2
---
+++ hw6/pca_generate.py
@@ -0,0 +1,33 @@
+import skimage.io
+import numpy as np
+import sys
+import os
+
+img_folder = sys.argv[1]
+test_imgi_index = int(sys.argv[2][:-4])
+
+# load all faces array
+image_X = []
+picture_path = img_folder + "/"
+for i in range(415):
+    single_img = skimage.io.imread(os.path.join(picture_path,str(i)+".jpg"))
+    image_X.append(single_img)
+image_flat = np.reshape(image_X,(415,-1))
+mean_face = np.mean(image_flat,axis=0).astype(np.uint8)
+
+image_center = image_flat - mean_face
+
+print("image center shape",image_center.shape)
+
+U, S, V = np.linalg.svd(image_center.T, full_matrices=False)
+
+print("U shape",U.shape)
+print("S shape",S.shape)
+print("V shape",V.shape)
+
+weights = np.dot(image_center, U)
+
+top = 4
+
+recon = mean_face + np.dot(weights[test_imgi_index, :top], U[:, :top].T)
+skimage.io.imsave("reconstruction.jpg", recon.reshape(600,600,3).astype(np.uint8))
